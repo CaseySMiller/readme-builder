3318cc62283aa477db3398e5782c55e520373542
CaseySMiller <CaseySMiller@yahoo.com>
2022-05-25 20:12:42 -0600

    working properly - needs testing

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require('fs');
-const generateMarkdown = require("./utils/generateMarkdown")
+const makeMD = require("./utils/generateMarkdown");
+console.log(makeMD);
 
 // TODO: Create an array of questions for user input
-let questions = {};
+const questions = {};
 inquirer
 .prompt([
     {
@@ -21,6 +22,21 @@ inquirer
         type: "input",
         message: "Describe the install process.",
         name: "install"
+    },
+    {
+        type: "input",
+        message: "What is address of the installation video",
+        name: "installVid"
+    },
+    {
+        type: "input",
+        message: "What is address of the installation video display image",
+        name: "installImg"
+    },
+    {
+        type: "input",
+        message: "What is address of the screenshot",
+        name: "screenShot"
     },
     {
         type: "input",
@@ -60,18 +76,25 @@ inquirer
 .then ( (response) => {
     console.log(response);
     // send response data to questions object
-    Object.assign(questions, response);
-    // call write function here
+    // Object.assign(questions, response);
 
-})
+    // call write function here
+    writeToFile('README.md', response);
+
+    // console.log(licenseBadge);
+});
 
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, makeMD(data), (err) =>
+    err ? console.error(err) : console.log('Success!')
+);
+};
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+// function init() {}
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,71 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// // TODO: Create a function that returns a license badge based on which license is passed in
+// // If there is no license, return an empty string
+// function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) {}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+// function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  // assign license badge to variable
+  licenseBadge = data.license == 'GNU GPLv13' ? '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)' : '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+  // return markdown
   return `# ${data.title}
+
+  ## Description
+  
+  ${data.descr}
+  
+  ## License
+  
+  Developed under the ${data.license} license ${licenseBadge}
+  
+  ## Table of Contents
+  
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [License](#license)
+  - [Test Instructions](#tests)
+  
+  ## Installation
+  
+  ${data.install}
+  
+  Install Video
+  [![Installation Video](${data.installImg})](${data.installVid} "Install Video")
+  
+  ## Usage
+  
+  ${data.usage}
+  
+  ## Screenshot
+  
+  ![screenshot](${data.screenShot})
+  
+  ## Contributing
+  
+  ${data.contrib}
+  
+  ## Tests
+  
+  ${data.testInstr}
+  
+  ---
+  
+  ## My GitHub
+  
+  [${data.gitUsr}](https://github.com/${data.gitUsr})
+  
+  ## Email me
+  
+  [${data.email}](mailto:${data.email})
 
 `;
 }
